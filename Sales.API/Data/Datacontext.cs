be53bac189355372120500e4c607bf8146d56828
JORGE ALEXANDER MUNERA HURTADO <jorgemunera95495@correo.itm.edu.co>
2023-03-02 00:04:15 -0500

    Add data Context

---
+++ Sales.API/Data/Datacontext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class Datacontext : DbContext
+    {
+        public Datacontext(DbContextOptions<Datacontext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+
+    }
+}
